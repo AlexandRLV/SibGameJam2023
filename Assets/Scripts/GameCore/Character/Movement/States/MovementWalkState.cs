9d1fe6fc4775f5f8d764fd3796dcdb48ab2b64b5
Aleksandr Litskevich <a.litskevich@lightmap.com>
2023-11-05 17:08:59 +0400

    Fixed project version

--- Assets/Scripts/GameCore/Character/Movement/States/MovementWalkState.cs
+++ Assets/Scripts/GameCore/Character/Movement/States/MovementWalkState.cs
@@ -1,12 +1,15 @@
 ﻿using Common;
 using GameCore.Character.Animation;
+using UnityEngine;
 
 namespace GameCore.Character.Movement.States
 {
     public class MovementWalkState : MovementStateBase
     {
-        public override AnimationType AnimationType => AnimationType.Walk;
+        public override AnimationType AnimationType => _standStillTime > Const.Character.StandStillTime ? AnimationType.IdleWait : AnimationType.Walk;
         public override MovementStateType Type => MovementStateType.Walk;
+
+        private float _standStillTime;
 
         public MovementWalkState(CharacterMovement characterMovement) : base(characterMovement)
         {
@@ -19,11 +22,15 @@ namespace GameCore.Character.Movement.States
 
         public override void Update()
         {
+            _standStillTime += Time.deltaTime;
             if (!movement.IsControlledByPlayer) return;
             
             var input = movement.InputState.moveVector;
             if (input.magnitude > 1f)
                 input = input.normalized;
+
+            if (input != Vector2.zero)
+                _standStillTime = 0f;
 
             float speed = parameters.speed * moveValues.SpeedMultiplier;
             if (movement.InputState.sneak.IsHold())
