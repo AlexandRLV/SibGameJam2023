2c003e4a6059458fb62d3c98a07bcb00336cbc55
Aleksandr Litskevich <a.litskevich@lightmap.com>
2023-11-06 19:30:11 +0400

    Signalling cat lookAround

--- Assets/Scripts/GameCore/Enemies/EnemyController.cs
+++ Assets/Scripts/GameCore/Enemies/EnemyController.cs
@@ -5,6 +5,8 @@ using GameCore.Sounds;
 using LocalMessages;
 using System.Collections;
 using System.Collections.Generic;
+using GameCore.Camera;
+using GameCore.Enemies;
 using UnityEditor;
 using UnityEngine;
 
@@ -59,7 +61,7 @@ public class EnemyController : MonoBehaviour
 
     public void Init(List<Waypoint> movePoints)
     {
-        enemyScan = GetComponent<EnemyTargetScaner>();
+        enemyScan = GetComponentInChildren<EnemyTargetScaner>();
         enemyMovement = GetComponent<EnemyMovement>();
         enemyFOV = GetComponentInChildren<EnemyFOV>();
         markController = GetComponentInChildren<MarkController>();
@@ -130,7 +132,8 @@ public class EnemyController : MonoBehaviour
     private void LateUpdate()
     {
         enemyFOV.DrawFOV(enemyScan.ViewDistance, enemyScan.ViewAngle, enemyScan.ObstacleLayer);
-        if (Camera.main != null) markController.LookAt(Camera.main.transform);
+        var cameraService = GameContainer.InGame.Resolve<GameCamera>();
+        if (cameraService != null && cameraService.Camera != null) markController.LookAt(cameraService.Camera);
     }
 
     private void OnPlayerDetected(ref PlayerDetectedMessage value)
@@ -139,8 +142,13 @@ public class EnemyController : MonoBehaviour
         enemyMovement.MoveToTarget(value.PlayerPosition);
         enemyFOV.SetColor(alertConeColor);
         markController.SetExclamationMark();
-    }
 
+        var lookIk = GetComponentInChildren<LookAtIK>();
+        if (lookIk != null)
+        {
+            lookIk.SetTarget(value.PlayerPosition);
+        }
+    }
     
     public void FoundPlayer(CharacterMovement movement)
     {
---
+++ Assets/Scripts/GameCore/Enemies/LookAtIK.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GameCore.Enemies
+{
+    public class LookAtIK : MonoBehaviour
+    {
+        [SerializeField] private Transform _head;
+
+        [SerializeField] private float _lookAtDistance;
+        [SerializeField] private float _lookAtRange;
+        [SerializeField] private float _lookAtSpeed;
+
+        private bool _hasTarget;
+
+        private void LateUpdate()
+        {
+            if (_hasTarget)
+                return;
+            
+            float t = Mathf.Sin(Time.time * _lookAtSpeed);
+
+            var position = transform.position + transform.forward * _lookAtDistance;
+            var right = transform.right * (t * _lookAtRange);
+            position += right;
+            position.y = _head.position.y;
+            _head.LookAt(position);
+        }
+
+        public void SetTarget(Vector3 target)
+        {
+            _hasTarget = true;
+            _head.LookAt(target);
+        }
+    }
+}
