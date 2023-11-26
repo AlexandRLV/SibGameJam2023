0c03c8d80707fec10a86ab0bbb316237e96e3817
Alexander Litskevich <a.litskevich@chillgaming.net>
2023-11-27 00:12:51 +0400

    Fixed cactus

--- Assets/Scripts/GameCore/LevelObjects/InteractiveObjects/CactusInteractiveObject.cs
+++ Assets/Scripts/GameCore/LevelObjects/InteractiveObjects/CactusInteractiveObject.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Common.DI;
 using GameCore.Character.Animation;
 using GameCore.LevelObjects.Abstract;
@@ -9,18 +10,13 @@ namespace GameCore.LevelObjects.InteractiveObjects
 {
     public class CactusInteractiveObject : InteractiveObject
     {
+        private const float AnimateTime = 0.5f;
+        
         public override AnimationType InteractAnimation => AnimationType.Eat;
         public override InteractiveObjectType Type => InteractiveObjectType.Cactus;
         public override Vector3 CheckPosition => transform.position;
 
         [Inject] private LocalMessageBroker _messageBroker;
-
-        private Vector3 _startPosition;
-        private Vector3 _targetPosition;
-        private Vector3 _startScale;
-        private Vector3 _endScale;
-        private bool _canStart;
-        private bool _isFinished;
 
         protected override void OnInitialize()
         {
@@ -29,55 +25,50 @@ namespace GameCore.LevelObjects.InteractiveObjects
 
         public override void Interact()
         {
-            if (IsUsed) return;
+            Debug.Log("Interact with cactus");
+            if (IsUsed)
+            {
+                Debug.Log("Already interacted");
+                return;
+            }
 
             IsUsed = true;
+            Debug.Log("Set interacted to true, start animation");
             
             var message = new CactusFoundMessage();
             _messageBroker.Trigger(ref message);
-        
-            _startScale = transform.localScale;
-            _endScale = Vector3.zero;
-            
-            _startPosition = transform.position;
-            _targetPosition = Movement.gameObject.transform.position;
-            
-            _canStart = true;
-            
+
+            StartCoroutine(Animate(AnimateTime, Movement.gameObject.transform.position));
+
             OnPlayerExit();
         }
 
         public override void InteractWithoutPlayer(Vector3 playerPosition)
         {
             Debug.Log("Trigger cactus found message");
+            IsUsed = true;
+            
             var message = new CactusFoundMessage();
             _messageBroker.Trigger(ref message);
-        
-            _startScale = transform.localScale;
-            _endScale = Vector3.zero;
             
-            _startPosition = transform.position;
-            _targetPosition = playerPosition;
-            
-            _canStart = true;
+            StartCoroutine(Animate(AnimateTime, playerPosition));
         }
 
-        private void Update()
+        private IEnumerator Animate(float time, Vector3 targetPosition)
         {
-            if (!_canStart || _isFinished) return;
-            if (Movement == null)
+            float timer = 0f;
+            var startPosition = transform.position;
+            var startScale = transform.localScale;
+            
+            while (timer < time)
             {
-                _isFinished = true;
-                gameObject.SetActive(false);
-                return;
+                timer += Time.deltaTime;
+                float t = timer / time;
+                transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+                transform.localScale = Vector3.Lerp(startScale, Vector3.zero, t);
+                yield return null;
             }
-
-            transform.position = Vector3.Lerp(_startPosition, _targetPosition, Time.deltaTime * 2f);
-            transform.localScale = Vector3.Lerp(_startScale, _endScale, Time.deltaTime * 20f);
-
-            if (Vector3.Distance(_startPosition, _targetPosition) > 0.5f) return;
-
-            _isFinished = true;
+            
             gameObject.SetActive(false);
         }
     }
