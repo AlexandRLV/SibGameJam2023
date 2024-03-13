d70302f94e7a8eab936b551755b5c4a41d1ff733
Alexander Litskevich <a.litskevich@chillgaming.net>
2024-03-13 21:47:20 +0400

    Added rotator and floater to cheese

---
+++ Assets/Scripts/GameCore/Common/FloatingRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GameCore.Common
+{
+    public class FloatingRotator : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0f, _rotationSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
