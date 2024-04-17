86ceeb66a41ed3673258de45da963c425d430f77
Alexander Litskevich <a.litskevich@chillgaming.net>
2024-04-17 11:10:42 +0400

    Improved navmesh

---
+++ Assets/Scripts/GameCore/LevelObjects/NavMeshBakeObjectsDisabler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GameCore.LevelObjects
+{
+    public class NavMeshBakeObjectsDisabler : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _objectsToDisable;
+
+        [ContextMenu("Disable objects")]
+        private void DisableObjects()
+        {
+            foreach (var target in _objectsToDisable)
+            {
+                target.SetActive(false);
+            }
+        }
+
+        [ContextMenu("Enable objects")]
+        private void EnableObjects()
+        {
+            foreach (var target in _objectsToDisable)
+            {
+                target.SetActive(true);
+            }
+        }
+    }
+}
